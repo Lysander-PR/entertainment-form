d123cb1c6a85c449fb77f8e0be22e4e304b405e4
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2026-02-03 14:20:38 -0600

    add colProps to SchemaEntertainment interface for enhanced layout control

--- src/types/interfaces/schemas.interface.ts
+++ src/types/interfaces/schemas.interface.ts
@@ -1,4 +1,4 @@
-import type { DatePickerProps, InputNumberProps, InputProps } from "antd";
+import type { ColProps, DatePickerProps, InputNumberProps, InputProps } from "antd";
 import type { TypeRenderer } from "@/types/enums/type-renderer.enum";
 import type { CustomDraggerProps } from "./custom-dragger.interface";
 import type { CustomSelectProps } from "./custom-select.interface";
@@ -7,6 +7,7 @@ export interface SchemaEntertainment<T> {
     field: T;
     type: TypeRenderer;
     label?: string;
+    colProps?: ColProps
     datePickerProps?: DatePickerProps;
     draggerProps?: CustomDraggerProps;
     inputProps?: InputProps;
