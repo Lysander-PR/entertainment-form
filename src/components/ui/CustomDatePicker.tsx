06a70bc551aff89fe04dd4a4dac41b518f63220a
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2026-02-03 21:48:14 -0600

    add: integrate dayjs for date handling in CustomDatePicker component

--- src/components/ui/CustomDatePicker.tsx
+++ src/components/ui/CustomDatePicker.tsx
@@ -1,11 +1,27 @@
 import { DatePicker } from "antd"
 import type { DatePickerProps } from "antd"
+import dayjs from "dayjs"
 
-export const CustomDatePicker = (datePickerProps: DatePickerProps) => {
+export const CustomDatePicker = ({
+  value,
+  ...datePickerProps
+}: DatePickerProps) => {
+  let normalizedValue: dayjs.Dayjs | undefined;
+
+  if (value !== null && value !== undefined) {
+    if (Array.isArray(value)) {
+      value = value[0];
+    }
+
+    normalizedValue = dayjs(value);
+  }
+
+
   return (
     <DatePicker
         className="w-full"
         format="DD / MM / YYYY"
+        value={normalizedValue}
         {...datePickerProps}
     />
   )
