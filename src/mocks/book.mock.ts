d960e33d160938a57ecd35552940cb0245406ec5
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2026-02-10 21:26:31 -0600

    fix: update mockBook details to the current book i'm reading

--- src/mocks/book.mock.ts
+++ src/mocks/book.mock.ts
@@ -1,8 +1,8 @@
 export const mockBook = {
-  author: "Brandon Sanderson",
-  cowriter: "Dan Wells",
-  publisher: "Tor Books",
-  releaseDate: 1784095200000, // 2026-07-15T06:00:00.000Z
-  title: "Skyward Legacy 2",
-  coverImage: "https://example.com/assets/books/covers/skyward-legacy-2.jpg"
+  author: "Andy Weir",
+  cowriter: "",
+  publisher: "Ballantine Books",
+  releaseDate: 1620172800000, // 2021-05-04T00:00:00.000Z
+  title: "Project Hail Mary",
+  coverImage: "https://example.com/assets/books/covers/project-hail-mary.jpg"
 }
