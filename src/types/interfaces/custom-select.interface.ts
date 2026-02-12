807f7d87bc0cbad6905359f77b7c26dd078dda20
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2026-02-12 01:02:30 -0600

    propertie options from CustomSelectProps interface typed with DefaultOptionType from antd library

--- src/types/interfaces/custom-select.interface.ts
+++ src/types/interfaces/custom-select.interface.ts
@@ -1,5 +1,6 @@
 import type { SelectProps } from "antd";
+import type { DefaultOptionType } from "antd/es/select";
 
 export interface CustomSelectProps extends Omit<SelectProps, 'options'> {
-  options: NonNullable<SelectProps['options' ]>;
+  options: DefaultOptionType[];
 }
