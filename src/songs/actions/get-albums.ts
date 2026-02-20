b34877a164153251311ff66dc026d4409aae3444
Lysander-PR <lysanderpr1@gmail.com>
2026-02-19 21:03:25 -0600

    Add logging for album retrieval in getAlbums function

--- src/songs/actions/get-albums.ts
+++ src/songs/actions/get-albums.ts
@@ -2,5 +2,6 @@ import { albums } from "@/mocks/song.mock"
 import type { AlbumResponse } from "@/songs/types/interfaces/album-response.interface"
 
 export const getAlbums = (id: string): AlbumResponse[] => {
+    console.log(`Getting albums for song with id ${id}...`);
     return albums
 }
