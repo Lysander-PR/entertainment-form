1748e5f0d41fe7608c2dc3589e890992e66e053a
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2026-08-03 16:42:24 -0600

    Add access token management functions and integrate token retrieval in axios requests

--- src/api/entertainment.api.ts
+++ src/api/entertainment.api.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import { getAccessToken } from '@/api/auth-token';
 
 export const entertainmentApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        'Content-Type': 'application/json'
+    baseURL: import.meta.env.VITE_API_URL
+});
+
+entertainmentApi.interceptors.request.use((config) => {
+    const token = getAccessToken();
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
 });
